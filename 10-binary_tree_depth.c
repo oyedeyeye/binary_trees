b1ddc1f2e19cc12dfaa51a8e911b08b72306c64a
Hezekiah Oyemade <oyedey@gmail.com>
2022-11-18 05:01:50 +0100

    mandatory task

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - calls recursive_depth to return the depth
+ * of a node in a binary tree
+ *
+ * @tree: tree
+ * Return: depth of the tree or 0 if tree is NULL;
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	return (recursive_depth(tree));
+}
+
+/**
+ * recursive_depth - measures the depth of a node in a binary tree
+ *
+ * @tree: tree
+ * Return: depth of a node in a binary tree
+ */
+size_t recursive_depth(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (-1);
+	return (recursive_depth(tree->parent) + 1);
+}
